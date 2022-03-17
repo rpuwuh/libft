350aa84b249ef902f1c783437bd4fba1ca4d0942
Beedrill Poetess <bpoetess@oa-e1.msk.21-school.ru>
2022-03-17 17:52:58 +0300

    rm a temp file

--- ft_strlen.c
+++ ft_strlen.c
@@ -3,20 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlen.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bpoetess <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: bpoetess <bpoetess@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/07 17:01:23 by bpoetess          #+#    #+#             */
-/*   Updated: 2021/10/09 18:32:10 by bpoetess         ###   ########.fr       */
+/*   Updated: 2022/01/05 22:16:45 by bpoetess         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include "libft.h"
 
-size_t	ft_strlen(const char	*s)
+size_t	ft_strlen(const char *s)
 {
 	size_t	i;
 
 	i = 0;
-	while (s[i++])
-		;
-	return (--i);
+	while (s && s[i])
+		i++;
+	return (i);
 }
